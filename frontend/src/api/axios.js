64ee97a10119ca48e7755bfa76e810ad43b85f31
khalid <khalid@gmail.com>
2026-05-24 21:31:58 +0300

    update for all cors

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,22 +6,12 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 403) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  },
-);
 
 export default api;
